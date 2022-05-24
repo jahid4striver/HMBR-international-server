b7547069dbacad0d07cdd553d74a5ba243c0a3e1
jahid4striver <jahidhassn701@gmail.comr>
2022-05-24 13:10:39 +0600

    Database Connecteed

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require('express')
 const cors = require('cors');
-const app = express()
+const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vsynq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect()
+        console.log('DB Connected');
+        const toolsCollection = client.db("HMBR_TOOLS").collection("tools");
+        
+
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
-  res.send('Makute International Server Is Running')
+    res.send('Makute International Server Is Running')
 })
 
 app.listen(port, () => {
-  console.log(`Makete app listening on port ${port}`)
+    console.log(`Makete app listening on port ${port}`)
 })
