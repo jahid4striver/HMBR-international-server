58d831a0b2877e49c834ffa381c55885eba75183
jahid4striver <jahidhassn701@gmail.comr>
2022-05-28 01:16:33 +0600

    Basic JWT Created

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY)
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            res.status(403).send({ message: 'Forbidden Access'})
+        }
+        req.decoded = decoded
+        next();
+    });
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vsynq.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,33 +40,33 @@ const run = async () => {
         const ordersCollection = client.db("HMBR_TOOLS").collection("orders");
         const usersCollection = client.db("HMBR_TOOLS").collection("users");
 
-        app.get('/tools', async(req, res)=>{
-            const tools= await toolsCollection.find().sort({$natural:-1}).toArray();
+        app.get('/tools', verifyJWT, async (req, res) => {
+            const tools = await toolsCollection.find().sort({ $natural: -1 }).toArray();
             res.send(tools);
         })
-        app.post('/tools', async(req, res)=>{
-            const tool= req.body
-            const result= await toolsCollection.insertOne(tool);
+        app.post('/tools', async (req, res) => {
+            const tool = req.body
+            const result = await toolsCollection.insertOne(tool);
             res.send(result);
         })
-        app.get('/reviews', async(req, res)=>{
-            const reviews= await reviewsCollection.find().sort({$natural:-1}).toArray();
+        app.get('/reviews', async (req, res) => {
+            const reviews = await reviewsCollection.find().sort({ $natural: -1 }).toArray();
             res.send(reviews);
         })
 
-        app.get('/tools/:id', async(req, res)=>{
-            const id= req.params.id;
-            const query={_id: ObjectId(id)};
-            const result= await toolsCollection.findOne(query);
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await toolsCollection.findOne(query);
             res.send(result);
         });
 
-        app.delete('/tools/:id', async(req, res)=>{
-            const id= req.params.id;
-            const query= {_id: ObjectId(id)};
-            const result= await toolsCollection.deleteOne(query);
+        app.delete('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await toolsCollection.deleteOne(query);
             res.send(result);
-        } )
+        })
 
         // app.put('/toolsqytupdate/:id', async(req, res)=>{
         //     const id= req.params.id;
@@ -79,120 +94,121 @@ const run = async () => {
         // });
 
 
-        app.get('/orders/:id', async(req, res)=>{
-            const id= req.params.id;
-            const query={_id: ObjectId(id)};
-            const result= await ordersCollection.findOne(query);
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.findOne(query);
             res.send(result);
         });
 
-        app.patch('/orders/:id', async(req, res)=>{
-            const id= req.params.id;
-            const payment= req.body;
-            const filter={_id: ObjectId(id)};
-            const updateDoc={
-                $set:{
-                    paid:true,
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    paid: true,
                     paymentId: payment.paymentId,
-                    status:'Pending'
+                    status: 'Pending'
                 }
             }
-            const result= await ordersCollection.updateOne(filter,updateDoc);
+            const result = await ordersCollection.updateOne(filter, updateDoc);
             res.send(result);
         });
 
-        app.put('/manageorders/:id', async(req, res)=>{
-            const id= req.params.id;
-            const payment= req.body;
-            const filter={_id: ObjectId(id)};
-            const options= {upsert: true}
-            const updateDoc={
+        app.put('/manageorders/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true }
+            const updateDoc = {
                 $set: payment,
             }
-            const result= await ordersCollection.updateOne(filter,updateDoc, options);
+            const result = await ordersCollection.updateOne(filter, updateDoc, options);
             res.send(result);
         });
 
-        app.delete('/orders/:id', async(req, res)=>{
-            const id= req.params.id;
-            const filter= {_id: ObjectId(id)};
-            const result= await ordersCollection.deleteOne(filter);
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(filter);
             res.send(result);
-        } )
+        })
 
-        app.post('/orders', async(req, res)=>{
-            const orders= req.body;
-            const result= await ordersCollection.insertOne(orders);
-            res.send(result);
-        });
-
-        app.get('/orders', async(req, res)=>{
-            const email= req.query.email;
-            const filter= {email: email};
-            const result= await ordersCollection.find(filter).toArray();
+        app.post('/orders', async (req, res) => {
+            const orders = req.body;
+            const result = await ordersCollection.insertOne(orders);
             res.send(result);
         });
 
-        app.get('/allorders', async(req, res)=>{
-            const result= await ordersCollection.find().toArray();
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const filter = { email: email };
+            const result = await ordersCollection.find(filter).toArray();
             res.send(result);
         });
 
-        app.post('/create-payment-intent', async(req, res)=>{
-            const purchaseOrder= req.body;
-            const amount= purchaseOrder.amount*100;
-            const paymentIntent= await stripe.paymentIntents.create({
+        app.get('/allorders', async (req, res) => {
+            const result = await ordersCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const purchaseOrder = req.body;
+            const amount = purchaseOrder.amount * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
                 currency: 'usd',
                 payment_method_types: ['card']
             });
-            res.send({clientSecret: paymentIntent.client_secret})
+            res.send({ clientSecret: paymentIntent.client_secret })
         });
 
-        app.post('/reviews', async(req,res)=>{ 
-            const review= req.body;
-            const result= await reviewsCollection.insertOne(review);
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
             res.send(result);
         });
 
-        app.post('/users', async(req, res)=>{
-            const user= req.body;
-            const result= await usersCollection.insertOne(user);
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
             res.send(result);
         })
 
-        app.put('/users/:email', async(req, res)=>{
-            const user= req.body;
-            const email= req.params.email;
-            const filter= {email: email};
+        app.put('/users/:email', async (req, res) => {
+            const user = req.body;
+            const email = req.params.email;
+            const filter = { email: email };
             const options = { upsert: true };
-            const updateDoc={
-                $set:user,
+            const updateDoc = {
+                $set: user,
             }
-            const result= await usersCollection.updateOne(filter,updateDoc,options);
-            res.send(result);
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
         });
 
 
-        app.get('/users/', async(req, res)=>{
-            const email= req.query.email;
-            const filter= {email: email};
-            const result= await usersCollection.findOne(filter);
+        app.get('/users/', async (req, res) => {
+            const email = req.query.email;
+            const filter = { email: email };
+            const result = await usersCollection.findOne(filter);
             res.send(result);
         })
-        app.get('/allusers', async(req, res)=>{
-            const result= await usersCollection.find().toArray();
+        app.get('/allusers', async (req, res) => {
+            const result = await usersCollection.find().toArray();
             res.send(result);
         })
 
-        app.delete('/users/:id', async(req, res)=>{
-            const id= req.params.id;
-            const filter= {_id: ObjectId(id)};
-            const result= await usersCollection.deleteOne(filter);
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(filter);
             res.send(result);
-        } )
+        })
 
-        
+
 
 
 
